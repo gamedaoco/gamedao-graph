3c414ef264dd2e8852e34341a9b8d4780051d4d2
Persqual <pascal@zero.io>
2022-03-15 00:13:33 +0100

    Fixed naming of env var.

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,4 @@
 import {codec} from "@subsquid/ss58";
 
 // Exports
-export const addressCodec = codec(parseInt(process.env.NETWORK_PREFIX ?? "0"));
+export const addressCodec = codec(parseInt(process.env.NETWORK_ADDRESS_PREFIX ?? "0"));
